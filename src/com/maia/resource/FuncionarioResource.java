43cde3dea7d4a6ac4e04675897067e3c02e2eb14
Dowglas Maia <dowglasmaia@gmail.com>
2019-01-12 11:09:27 -0200

    Impl -> Buscar por Funcionarios, Ativo e Inativos

--- src/com/maia/resource/FuncionarioResource.java
+++ src/com/maia/resource/FuncionarioResource.java
@@ -1,5 +1,84 @@
 package com.maia.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import com.maia.model.Departamento;
+import com.maia.model.Funcionario;
+import com.maia.model.enums.Status;
+
+@Path("/funcionarios")
 public class FuncionarioResource {
+
+	private int id = 1;
+	private Map<Integer, Funcionario> funcionarios = new ConcurrentHashMap<>();
+
+	public FuncionarioResource() {
+		this.adicionarFuncionario();
+	}
+
+	private void adicionarFuncionario() {
+
+	}
+
+	/*Buscar Por Ativos*/
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Departamento getFuncionario(@DefaultValue("ativo") @QueryParam("status") String status) {
+
+		List<Funcionario> lista = new ArrayList<Funcionario>();
+
+		for (Object key : funcionarios.keySet()) {
+			Funcionario func = funcionarios.get(key);
+
+			if (func.getStatus().getDescricao().equalsIgnoreCase(status)) {
+				lista.add(func);
+			}
+		}
+
+		Departamento departamento = new Departamento(lista);
+		return departamento;
+	}
+
+	/*Buscar Por inativos*/
+	@GET
+	@Path("/inativos")
+	@Produces(MediaType.APPLICATION_XML)
+	public Departamento getFuncionariosInativos() {
+
+		List<Funcionario> lista = new ArrayList<>();
+
+		for (Object key : funcionarios.keySet()) {
+			Funcionario func = funcionarios.get(key);
+
+			if (func.getStatus() == Status.I) {
+				lista.add(func);
+			}
+		}
+		Departamento departamento = new Departamento(lista);
+		return departamento;
+
+	}
+	
+	/*Buscar Por Salario*/
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Departamento getFuncionarioPorSalario() {
+		
+		
+		
+		return null;		
+	}
+	
+	/*Novo Funcionario*/
+	@POST
+	public void novoFuncionario() {
+		
+	}
 
 }
